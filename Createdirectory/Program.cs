1248520cdbf4f5490ddf1377b85e9eddb646c494
Opilane TTHK <opilane@tthk.ee>
2020-11-18 14:57:57 +0200

    first commit

---
+++ Createdirectory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Createdirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreatefolderOne();
+        }
+
+        public static void CreatefolderOne()
+        {
+            string roothPath = @"C:\Users\opilane\samples";
+            string folderName = "playerOne";
+            string fullFolderPath = Path.Combine(roothPath, folderName);
+            Console.WriteLine(fullFolderPath);
+            if (Directory.Exists(fullFolderPath))
+            {
+                Console.WriteLine("The folder already exists.");
+            }else
+            {
+                Directory.CreateDirectory(fullFolderPath);
+            }
+        }
+    }
+}
